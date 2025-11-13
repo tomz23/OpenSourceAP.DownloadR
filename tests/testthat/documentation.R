b38638399447b16a95e730d5d2a288d613bd45f5
justus.boeker@t-online.de <justus.boeker@t-online.de>
2025-11-13 12:24:47 +0100

    Remove obsolete file

--- tests/testthat/documentation.R
+++
@@ -1,4 +0,0 @@
-# Creating a package structure to enable the use of the document() function
-
-#usethis::create_package("/Users/justus/Coding/R/Uni /Machine Learning /OpenSourceAP.DownloadR")
-devtools::document()
